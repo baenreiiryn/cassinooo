ea52f8fbc0285016c99f5fda037b2d0651175e18
baenreiiryn <baenreiiryn@gmail.com>
2026-08-28 13:29:47 +0900

    Remove card back appearance settings

--- scripts/module-settings.js
+++ scripts/module-settings.js
@@ -1,9 +1,9 @@
 import { CasinoSettings } from "../applications/casino-settings.js";
-import { BACKGROUND_SETTINGS, CARD_BACK_SETTINGS } from "./backgrounds.js";
+import { BACKGROUND_SETTINGS } from "./backgrounds.js";
 import { MODULE_ID } from "./state.js";
 
 export function registerModuleSettings() {
-  for (const settingKey of [...Object.values(BACKGROUND_SETTINGS), ...Object.values(CARD_BACK_SETTINGS)]) {
+  for (const settingKey of Object.values(BACKGROUND_SETTINGS)) {
     game.settings.register(MODULE_ID, settingKey, {
       name: settingKey,
       hint: "",
@@ -17,7 +17,7 @@ export function registerModuleSettings() {
   game.settings.registerMenu(MODULE_ID, "tableBackgrounds", {
     name: "Aparência das mesas",
     label: "Configurar aparência",
-    hint: "Escolha os fundos das mesas e os versos das cartas do Blackjack e Beholdem.",
+    hint: "Escolha os fundos das mesas do Cassinooo. As cartas usam as texturas do modelo 3D incluído no módulo.",
     icon: "fa-solid fa-images",
     type: CasinoSettings,
     restricted: true
