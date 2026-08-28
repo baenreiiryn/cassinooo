31ab40995893e1c33c9ae6cab2484e2d2e460235
baenreiiryn <baenreiiryn@gmail.com>
2026-08-28 11:30:39 +0900

    feat: register reusable casino 3D models

---
+++ scripts/model-library.js
@@ -0,0 +1,12 @@
+export const CASINO_3D_MODELS = Object.freeze({
+  dragonDice: Object.freeze({
+    d4: "modules/cassinooo/assets/models/d4.obj",
+    d6: "modules/cassinooo/assets/models/d6.obj",
+    d8: "modules/cassinooo/assets/models/d8.obj",
+    cup: "modules/cassinooo/assets/models/dice-cup.obj"
+  }),
+  card: "modules/cassinooo/assets/models/card.obj",
+  rouletteWheel: "modules/cassinooo/assets/models/roulette-wheel.obj",
+  rouletteBall: "modules/cassinooo/assets/models/roulette-ball.obj",
+  coin: "modules/cassinooo/assets/models/coin.obj"
+});
