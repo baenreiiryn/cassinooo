cd59250cb3a1ee2f8a8c2a6ae3229b2aa5928e4a
baenreiiryn <baenreiiryn@gmail.com>
2026-08-29 08:06:51 +0900

    Lazy-load Dragon Dice 3D renderer after window opens

--- scripts/dragon-dice-model-renderer.js
+++ scripts/dragon-dice-model-renderer.js
@@ -1,1 +1,18 @@
-export { DragonDiceWebGL as DragonDiceModelRenderer } from "./dragon-dice-webgl.js";
+export class DragonDiceModelRenderer {
+  constructor(canvas){
+    this.canvas=canvas;
+    this.renderer=null;
+  }
+
+  async init(){
+    const { DragonDiceWebGL } = await import("./dragon-dice-webgl.js");
+    const renderer = await new DragonDiceWebGL(this.canvas).init();
+    this.renderer = renderer;
+    return renderer;
+  }
+
+  dispose(){
+    this.renderer?.dispose?.();
+    this.renderer=null;
+  }
+}
