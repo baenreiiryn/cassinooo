aad9daf422207bd8e95ff199eb121eb295364486
baenreiiryn <baenreiiryn@gmail.com>
2026-08-28 22:38:46 +0900

    Fix 0.14 model data imports to complete compact chunks

--- scripts/model-data.js
+++ scripts/model-data.js
@@ -1,11 +1,15 @@
+import card0 from "./model-data/cards-v140-00.js";
+import card1 from "./model-data/cards-v140-01.js";
+import card2 from "./model-data/cards-v140-02.js";
+import card3 from "./model-data/cards-v140-03.js";
+import card4 from "./model-data/cards-v140-04.js";
+
+import dice from "./model-data/dice-v140.js";
+
 import cup0 from "./model-data/cup-00.js";
 import cup1 from "./model-data/cup-01.js";
 import cup2 from "./model-data/cup-02.js";
-import dice0 from "./model-data/dice-00.js";
-import cards0 from "./model-data/cards-00.js";
-import cards1 from "./model-data/cards-01.js";
-import cards2 from "./model-data/cards-02.js";
 
+export const CARD_MODEL_DATA = JSON.parse([card0, card1, card2, card3, card4].join(""));
+export const DICE_MODEL_DATA = JSON.parse(dice);
 export const CUP_MODEL_DATA = JSON.parse([cup0, cup1, cup2].join(""));
-export const DICE_MODEL_DATA = JSON.parse(dice0);
-export const CARD_MODEL_DATA = JSON.parse([cards0, cards1, cards2].join(""));
