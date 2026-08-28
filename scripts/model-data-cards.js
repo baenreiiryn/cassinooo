e1e25eeb4f8ec7435c355c9d12503246727a7b32
baenreiiryn <baenreiiryn@gmail.com>
2026-08-29 08:28:49 +0900

    refactor: isolate card model data

---
+++ scripts/model-data-cards.js
@@ -0,0 +1,7 @@
+import card0 from "./model-data/cards-v140-00.js";
+import card1 from "./model-data/cards-v140-01.js";
+import card2 from "./model-data/cards-v140-02.js";
+import card3 from "./model-data/cards-v140-03.js";
+import card4 from "./model-data/cards-v140-04.js";
+
+export const CARD_MODEL_DATA = JSON.parse([card0, card1, card2, card3, card4].join(""));
